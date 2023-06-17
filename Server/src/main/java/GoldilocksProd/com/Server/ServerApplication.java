b91345acd68f673f380ce5fe4f2d98ffff9ba601
alemneh1 <4alexmic@gmail.com>
2023-06-16 21:55:44 -0700

    Here i have finished creating a form that handle post request to S3 and display it on a angular card

--- Server/src/main/java/GoldilocksProd/com/Server/ServerApplication.java
+++ Server/src/main/java/GoldilocksProd/com/Server/ServerApplication.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class ServerApplication {
 
 	public static void main(String[] args) {
--- Server/src/main/java/GoldilocksProd/com/Server/controller/MusicController.java
+++ Server/src/main/java/GoldilocksProd/com/Server/controller/MusicController.java
@@ -5,6 +5,7 @@ import GoldilocksProd.com.Server.services.S3Service;
 import GoldilocksProd.com.Server.services.musicService.MusicProjectServicesImp;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -15,33 +16,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @RestController
 @RequiredArgsConstructor
-public class MusicController {
+@Slf4j
 
+public class MusicController {
+    private static final Logger logger = Logger.getLogger(MusicController.class.getName());
     private final MusicProjectServicesImp musicProjectServices;
     @Autowired
     private S3Service s3Service;
 
     @PostMapping(value = "/api/music-projects", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public MusicProject createMusicProject(@RequestParam("file") MultipartFile file,
+    public MusicProject createMusicProject(@RequestParam(value = "thumbnailImage", required = false) MultipartFile file,
                                            @RequestParam("projectType") String projectType,
                                            @RequestParam("title") String title,
                                            @RequestParam("year") int year,
                                            @RequestParam("musicianName") String musicianName,
                                            @RequestParam("musicDescription") String musicDescription,
-                                           @RequestParam("thumbnailImage") String thumbnailImage,
                                            @RequestParam("videoPath") String videoPath) {
         // Create a MusicProject instance and set its properties
         MusicProject musicProject = new MusicProject();
+
+        if (file != null) {
+            String originalFileName = file.getOriginalFilename();
+            //String contentType = file.getContentType();
+            //long fileSize = file.getSize();
+
+//            logger.info("Original File Name: " + originalFileName);
+//            logger.info("Content Type: " + contentType);
+//            logger.info("File Size: " + fileSize);
+
+            s3Service.addImage(file);
+            //make sure to change this to 5 years
+            musicProject.setThumbnailPath(s3Service.getPresignedImageUrl(originalFileName, 1800));
+        }
+
         musicProject.setProjectType(projectType);
         musicProject.setTitle(title);
         musicProject.setDate(year);
         musicProject.setMusicianName(musicianName);
         musicProject.setMusicDescription(musicDescription);
-        musicProject.setThumbnailPath(thumbnailImage);
         musicProject.setVideoPath(videoPath);
 
         // Save the MusicProject and return the saved entity
